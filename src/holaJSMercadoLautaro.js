d119b64b4dde3647ff5a49adeed28c4ddd77f72b
Iso Mercado <lauty.jose.mercado@gmail.com>
2026-04-23 15:53:10 -0300

    Archivo 2.0 JS de Mercado Lautaro

---
+++ src/holaJSMercadoLautaro.js
@@ -0,0 +1,9 @@
+var nombre = "Lautaro"; //no se usa, esta obsoleto, se reemplaza por let o const
+let apellido = "Mercado"; //se trabaja con let y const
+//const TEXTO = "Hola Mundo, segun";
+const TEXTO = `Hola Mundo, segun ${nombre} ${apellido}`;
+
+//console.log("Hola Mundo, segun " + nombre + " " + apellido);
+//console.log(`Hola Mundo, segun ${nombre} ${apellido}`);
+//console.log(TEXTO + nombre + apellido);
+console.log(TEXTO);
