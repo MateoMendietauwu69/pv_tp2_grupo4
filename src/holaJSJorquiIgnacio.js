07c5c138f50142f2568ca3bc44442e942ba99a76
Nacho <jorquiignacio@gmail.com>
2026-04-23 16:08:17 -0300

    Relleno el js - Ignacio Jorqui

---
+++ src/holaJSJorquiIgnacio.js
@@ -0,0 +1,9 @@
+var nombre = "Ignacio"; //no se usa, esta obsoleto, se reemplaza por let o const
+let apellido = "Jorqui"; //se trabaja con let y const
+//const TEXTO = "Hola Mundo, segun";
+const TEXTO = `Hola Mundo, segun ${nombre} ${apellido}`;
+
+//console.log("Hola Mundo, segun " + nombre + " " + apellido);
+//console.log(`Hola Mundo, segun ${nombre} ${apellido}`);
+//console.log(TEXTO + nombre + apellido);
+console.log(TEXTO);
