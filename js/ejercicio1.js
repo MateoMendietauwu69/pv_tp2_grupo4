3db1638eb414492309f01c5be0a85a9a504b427b
octaviolamas <octaviolamaspro1111@gmail.com>
2026-04-29 23:41:43 -0300

    Subo ejercicio1

--- js/ejercicio1.js
+++
@@ -1,3 +0,0 @@
-let integrantes = "Ignacio Jorqui, Octavio Lamas, Mateo Mendieta, Lautaro Mercado, Erick Neuman, David Palacios"
-
-console.log ("somos " + integrantes + " y estamos aprendiendo javascript.");
