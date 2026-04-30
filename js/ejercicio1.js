981d9e0c888fb701641640c84b6da914f9f7836d
Iso Mercado <lauty.jose.mercado@gmail.com>
2026-04-30 18:02:51 -0300

    Asistencia Mercado Lautaro

---
+++ js/ejercicio1.js
@@ -0,0 +1,5 @@
+const integrantes = "Mateo, David, Erick, Lautaro, Octavio e Ignacio";
+
+const mensaje = `Somos ${integrantes} y estamos aprendiendo javascript.`;
+
+console.log(mensaje);
---
+++ js/ejercicio20.js
@@ -0,0 +1,4 @@
+import { servicioDavidPalacios } from "../services/servicesDavidPalacios";
+import { servicioMateoMendieta } from "../services/servicesMateoMendieta";
+import { servicioErickNeuman } from "../pv_tp2_grupo4/services/servicioErickNeuman";
+import { servicioMateoMendieta } from "../services/servicesMercadoLautaro";
