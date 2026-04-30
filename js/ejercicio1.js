4415d57d797211136b33420c9c34aa5fddf3e79e
MateoMendietauwu69 <mendietamateo510@gmail.com>
2026-04-29 23:27:18 -0300

    Desarrollo ejercicio 1 , 10 y 16 - Mateo Mendieta

---
+++ js/ejercicio1.js
@@ -0,0 +1,3 @@
+let integrantes = "Ignacio Jorqui, Octavio Lamas, Mateo Mendieta, Lautaro Mercado, Erick Neuman, David Palacios"
+
+console.log ("somos " + integrantes + " y estamos aprendiendo javascript.");
