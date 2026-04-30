b018982fe3dcfbc7706c59a5d1d7663d0b253808
ericks.cpp <neumanerick75@gmail.com>
2026-04-30 17:36:05 -0300

    Agregue servicesErickNeuman.js. Erick Neuman

---
+++ js/ejercicio20.js
@@ -0,0 +1,2 @@
+import { servicioDavidPalacios } from "../services/servicesDavidPalacios";
+import { servicioErickNeuman } from "../services/servicesErickNeuman";
---
+++ services/servicesMateoMendieta.js
@@ -0,0 +1,7 @@
+export const servicioMateoMendieta = () =>
+{
+
+
+};
+
+//import {servicioMateoMendieta} from "";
