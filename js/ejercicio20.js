07cac9a191fea13f8ab167b5cdf3b0c9b097cc36
octaviolamas <octaviolamaspro1111@gmail.com>
2026-04-30 23:42:29 -0300

    Subo ejercicio20.js - Octavio Lamas

---
+++ js/ejercicio20.js
@@ -0,0 +1,5 @@
+import { servicioDavidPalacios } from "../services/servicesDavidPalacios";
+import { servicioMateoMendieta } from "../services/servicesMateoMendieta";
+import { servicioErickNeuman } from "../pv_tp2_grupo4/services/servicioErickNeuman";
+import { servicioMercadoLautaro } from "../services/servicesMercadoLautaro"; 
+import { servicioOctavioLamas } from "../services/servicesOctavioLamas.js";
