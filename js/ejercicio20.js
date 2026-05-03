e7c9a292e1ee5dc70209ce004c17013b1264749b
ericks.cpp <neumanerick75@gmail.com>
2026-05-02 21:28:15 -0300

    Agregue ejercicio24.html,.js y .css. Erick Neuman

--- js/ejercicio20.js
+++ js/ejercicio20.js
@@ -1,3 +1,4 @@
 import { servicioDavidPalacios } from "../services/servicesDavidPalacios";
 import { servicioMateoMendieta } from "../services/servicesMateoMendieta";
 import { servicioErickNeuman } from "../pv_tp2_grupo4/services/servicioErickNeuman";
+import { servicioMercadoLautaro } from "../services/servicesMercadoLautaro"; 
